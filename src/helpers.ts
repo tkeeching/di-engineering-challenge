e9d94864b2d9923f2e41488a96346a17de68f9ae
Kee Tee <tkeeching@gmail.com>
2025-10-27 14:13:16 +1100

    Update regex

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,5 @@
 export const isValidWikipediaUrl = (url: string): boolean => {
-    const wikiRegex = /^https:\/\/([a-z]{2})\.wikipedia\.org\/wiki\/[^\s]+$/i;
+    const wikiRegex = /^https:\/\/([a-z]{2}\.)?wikipedia\.org\/wiki\/[^\s]+$/i;
 
     return wikiRegex.test(url);
 }
