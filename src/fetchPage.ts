2f6c13b5c1c7222294fd9695745ebb7ab6abf676
Kee Tee <tkeeching@gmail.com>
2025-11-06 18:00:04 +1100

    Add headers

--- src/fetchPage.ts
+++ src/fetchPage.ts
@@ -2,7 +2,14 @@ import axios from "axios";
 
 export default async function fetchPage(url: string): Promise<string> {
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+                    "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                    "Chrome/142.0.0.0 Safari/537.36",
+            },
+        });
         return response.data;
     } catch (error) {
         if (error.response && error.response.status === 404) {
